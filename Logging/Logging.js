706fd39ecd8d21dc7c1c1386197a7e173a97420d
Marko Lokka <e1901043@edu.vamk.fi>
2020-04-16 21:30:22 +0300

    Updates made

--- Logging/Logging.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-
-function Logging() {}
-
-Logging.prototype.addToLogFile = function(str) {
-    
-};
-
-module.exports = Logging;
--- Uploader/Uploader.js
+++
@@ -1,7 +0,0 @@
-function Uploader() {
-
-    uploadWeather();
-    uplodateRaspberry();
-    
-}
-module.exports = Uploader;
